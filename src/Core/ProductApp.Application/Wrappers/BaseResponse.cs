78e08c803851cf5abe1feb6751bb21b9ad6d08c6
Ramazan Halid <ramazan.halid.35@gmail.com>
2022-03-04 19:23:32 +0300

    Wrappers (Response Models) added

---
+++ src/Core/ProductApp.Application/Wrappers/BaseResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductApp.Application.Wrappers
+{
+    public class BaseResponse
+    {
+        public Guid guid { get; set; }
+        public bool Success { get; set; }
+        public String Message { get; set; }
+    }
+}
