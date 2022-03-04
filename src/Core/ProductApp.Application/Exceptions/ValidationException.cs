88494a36f3e9a17ebb57505040f05c73c0219110
Ramazan Halid <ramazan.halid.35@gmail.com>
2022-03-04 19:16:41 +0300

    Custom exception which is ValidationException added

---
+++ src/Core/ProductApp.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductApp.Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        //I used "this" keyword here because the second constructor will be assigned automatically for the string value we wrote in "this"
+        public ValidationException() : this("Validation error occured")
+        {
+        }
+        public ValidationException(String Message) : base(Message)
+        {
+        }
+
+        public ValidationException(Exception ex) : this(ex.Message)
+        {
+        }
+    }
+}
