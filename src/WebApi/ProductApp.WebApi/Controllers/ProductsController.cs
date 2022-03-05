3a1f370e3db1f445a35b401ec2266dd2912e60ef
Ramazan Halid <ramazan.halid.35@gmail.com>
2022-03-05 22:59:29 +0300

    Api added

---
+++ src/WebApi/ProductApp.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApp.Application.Dto;
+using ProductApp.Application.Interfaces.Repository;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductApp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public IProductRepository productRepository { get; }
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var allList = await productRepository.GetAllAsync();
+            var result = allList.Select(i => new ProductViewDto()
+            {
+                Id = i.Id,
+                Name = i.Name
+            }).ToList();
+            return Ok(result);
+        }
+    }
+}
