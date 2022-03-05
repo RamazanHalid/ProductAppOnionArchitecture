ea9f2bf11a99e937358eb2556effc6c3b027d678
Ramazan Halid <ramazan.halid.35@gmail.com>
2022-03-05 22:59:12 +0300

    Generic repository and ProductRepository added

---
+++ src/ProductApp.Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using ProductApp.Application.Interfaces.Repository;
+using ProductApp.Domain.Entities;
+using ProductApp.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductApp.Persistence.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        public ProductRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
