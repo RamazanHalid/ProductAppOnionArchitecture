d360dc15448309ecf09993f947b8506a30b79821
Ramazan Halid <ramazan.halid.35@gmail.com>
2022-03-06 11:04:42 +0300

    Service registration added for Application layer

--- src/Core/ProductApp.Application/ServiceRegistration.cs
+++ src/Core/ProductApp.Application/ServiceRegistration.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace ProductApp.Application
 {
     public static class ServiceRegistration
     {
+        public static void AddApplicationRegistration(this IServiceCollection services)
+        {
+            var assm = Assembly.GetExecutingAssembly();
+            services.AddAutoMapper(assm);
+            services.AddMediatR(assm);
+        }
     }
 }
