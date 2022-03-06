00d30ac477629c810f898970f0db112093bcd465
Ramazan Halid <ramazan.halid.35@gmail.com>
2022-03-06 11:02:58 +0300

    GetAllProductQuery added for get all products

---
+++ src/Core/ProductApp.Application/Features/Queries/GetAllProducts/GetAllProductQuery.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using ProductApp.Application.Dto;
+using ProductApp.Application.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProductApp.Application.Features.Queries.GetAllProducts
+{
+    public class GetAllProductQuery : IRequest<List<ProductViewDto>>
+    { 
+        public class GetAllProductQueryHandler : IRequestHandler<GetAllProductQuery, List<ProductViewDto>>
+        {
+            private readonly IProductRepository _productRepository;
+
+            public GetAllProductQueryHandler(IProductRepository productRepository)
+            {
+                _productRepository = productRepository;
+            }
+
+            public async Task<List<ProductViewDto>> Handle(GetAllProductQuery request, CancellationToken cancellationToken)
+            {
+                var products = await _productRepository.GetAllAsync();
+                return products.Select(i => new ProductViewDto()
+                {
+                    Id = i.Id,
+                    Name = i.Name
+                }).ToList();
+            }
+        }
+    }
+}
