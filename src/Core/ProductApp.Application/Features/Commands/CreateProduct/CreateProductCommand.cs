ca2c9151ca1b5ca6fc0f0fe22a5b915b21d36cb1
Ramazan Halid <ramazan.halid.35@gmail.com>
2022-03-07 15:35:35 +0300

    Create new product added

---
+++ src/Core/ProductApp.Application/Features/Commands/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using MediatR;
+using ProductApp.Application.Interfaces.Repository;
+using ProductApp.Application.Wrappers;
+using ProductApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProductApp.Application.Features.Commands.CreateProduct
+{
+    public class CreateProductCommand : IRequest<ServiceResponse<Guid>>
+    {
+        public String Name { get; set; }
+        public decimal Value { get; set; }
+        public int Quantity { get; set; }
+
+
+        public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, ServiceResponse<Guid>>
+        {
+            private readonly IProductRepository _productRepository;
+            private readonly IMapper _mapper;
+            public CreateProductCommandHandler(IProductRepository productRepository, IMapper mapper)
+            {
+                _productRepository = productRepository;
+                _mapper = mapper;
+            }
+
+
+            public async Task<ServiceResponse<Guid>> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+            {
+                var product = _mapper.Map<Product>(request);
+                await _productRepository.AddAsync(product);
+                return new ServiceResponse<Guid>(product.Id);
+            }
+        }
+    }
+}
