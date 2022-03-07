412c424de39cf9e9744d776e2637cbab1a0d380b
Ramazan Halid <ramazan.halid.35@gmail.com>
2022-03-07 15:34:02 +0300

    Mapper implemented

--- src/Core/ProductApp.Application/Features/Queries/GetAllProducts/GetAllProductQuery.cs
+++ src/Core/ProductApp.Application/Features/Queries/GetAllProducts/GetAllProductQuery.cs
@@ -1,6 +1,8 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
 using ProductApp.Application.Dto;
 using ProductApp.Application.Interfaces.Repository;
+using ProductApp.Application.Wrappers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,25 +12,25 @@ using System.Threading.Tasks;
 
 namespace ProductApp.Application.Features.Queries.GetAllProducts
 {
-    public class GetAllProductQuery : IRequest<List<ProductViewDto>>
-    { 
-        public class GetAllProductQueryHandler : IRequestHandler<GetAllProductQuery, List<ProductViewDto>>
+    public class GetAllProductQuery : IRequest<ServiceResponse<List<ProductViewDto>>>
+    {
+        public class GetAllProductQueryHandler : IRequestHandler<GetAllProductQuery, ServiceResponse<List<ProductViewDto>>>
         {
             private readonly IProductRepository _productRepository;
+            private readonly IMapper _mapper;
 
-            public GetAllProductQueryHandler(IProductRepository productRepository)
+            public GetAllProductQueryHandler(IProductRepository productRepository, IMapper mapper)
             {
                 _productRepository = productRepository;
+                _mapper = mapper;
             }
 
-            public async Task<List<ProductViewDto>> Handle(GetAllProductQuery request, CancellationToken cancellationToken)
+            public async Task<ServiceResponse<List<ProductViewDto>>> Handle(GetAllProductQuery request, CancellationToken cancellationToken)
             {
                 var products = await _productRepository.GetAllAsync();
-                return products.Select(i => new ProductViewDto()
-                {
-                    Id = i.Id,
-                    Name = i.Name
-                }).ToList();
+                var viewModel = _mapper.Map<List<ProductViewDto>>(products);
+
+                return new ServiceResponse<List<ProductViewDto>>(viewModel);
             }
         }
     }
